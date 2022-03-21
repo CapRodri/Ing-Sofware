d6654c00bf59d0f010f6d3a1a8cedd4fe1417df6
CapRodri <91547958+CapRodri@users.noreply.github.com>
2022-03-21 13:05:20 -0400

    revisar

--- hola.java
+++ hola.java
@@ -2,6 +2,8 @@ public class hola{
 
 	public static void main(String[] args) {		
 		System.out.println("Saludos a todos");
+		System.out.println("Estoy feliz");
+		System.out.println("adios");
 	}
 
 }
